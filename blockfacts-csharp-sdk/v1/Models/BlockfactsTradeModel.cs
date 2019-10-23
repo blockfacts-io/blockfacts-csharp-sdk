44e902e28b9f1ec45f83967606451bad3e544d3f
Ognjen Gatalo <ognjeng95@gmail.com>
2019-10-23 09:57:45 +0200

    Updated BlockfactsTradeModel.cs

--- blockfacts-csharp-sdk/v1/Models/BlockfactsTradeModel.cs
+++ blockfacts-csharp-sdk/v1/Models/BlockfactsTradeModel.cs
@@ -11,7 +11,7 @@ namespace blockfacts_csharp_sdk.v1.Models
         public double price { get; set; }
         public double tradeSize { get; set; }
         public string makerTaker { get; set; }
-        public long tradeId { get; set; }
+        public string tradeId { get; set; }
         public string exchangeTime { get; set; }
         public long blockfactsTime { get; set; }
         public long epochExchangeTime { get; set; }
